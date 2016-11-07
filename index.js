c55b5b3701407b044611386925d1e41531b0e6eb
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-11-07 13:09:59 +0900

    edit toppage

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+const path = require('path');
+const PORT = 3337;
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/docs/index.html'));
+});
+app.use("/", express.static(path.join(__dirname, 'docs')));
+app.use("/assets", express.static(path.join(__dirname, 'docs/assets')));
+
+app.listen(PORT);
+console.log('listening on port ' + PORT);
